61f75b44dd2ae555ff74bd8f3dff28273156cba4
Ayanga Wethmini <140165014+AyangaWethmini@users.noreply.github.com>
2024-08-20 07:17:21 +0530

    q3 done

--- q3.scala
+++ q3.scala
@@ -4,5 +4,19 @@ object Q3 extends App{
 
     val toLower : String => String = (str : String) => str.toLowerCase()
 
-    
+    val formatNames: (String, String => String) => String = 
+    (name: String, formatFunction: String => String) => formatFunction(name)
+
+
+
+    println(formatNames("Benny", toUpper))
+    println(formatNames("Niroshan", name => toUpper(name.substring(0, 2) + name.substring(2))))
+    println(formatNames("Saman", toLower))
+    println(formatNames("Kumara", name => 
+      toUpper(name.substring(0, 1)) + 
+      toLower(name.substring(1, 5)) + 
+      toUpper(name.substring(5))
+    ))
+
+
 }
